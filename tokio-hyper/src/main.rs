3930766b6d73fb49e06589f5b9293c2dac1e193f
Rock <rockcreation7@gmail.com>
2021-01-19 23:04:48 +0800

    add post eg

--- tokio-hyper/src/main.rs
+++ tokio-hyper/src/main.rs
@@ -1,7 +1,9 @@
 
-extern crate hyper; 
  
-use hyper::{body::HttpBody as _, Client};
+
+extern crate hyper;  
+use hyper::{body::HttpBody as _, Client, Body, Method, Request, Uri, http::Error};
+ 
 use tokio::io::{self, AsyncWriteExt as _};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -16,5 +18,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         io::stdout().write_all(&chunk?).await?;
     }
 
+    post().await;
+
+    Ok(())
+}
+
+async fn post() -> Result<(),  Box<dyn std::error::Error + Send + Sync>> {
+
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri("http://httpbin.org/post")
+        .header("content-type", "application/json")
+        .body(Body::from(r#"{"library":"hyper"}"#))?;
+
+
+    let client = Client::new();
+
+    // POST it...
+    let mut resp = client.request(req).await?;
+
+    while let Some(chunk) = resp.body_mut().data().await {
+        io::stdout().write_all(&chunk?).await?;
+    }
+    
+    println!("Response: {}", resp.status());
+
+    // We'll send it in a second...
     Ok(())
 }
